9eae7926f453251f861f7a6d0760b937fcffe1c3
Ej <edzeljohnsinfuego@gmail.com>
2024-11-13 07:30:03 +0800

    fix: pest, fertilizer page

--- pest.php
+++ pest.php
@@ -1,20 +1,8 @@
 <?php
-// Connect to the database
-$host = 'localhost';
-$dbname = 'lagonoy_farmers';
-$username = 'root';
-$password = '';
-
-$conn = new mysqli($host, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
+require_once('config/constant.php');
 // Fetch all fertilizers from the database
 $sql = "SELECT * FROM pest WHERE is_archived = 0";
-$result = $conn->query($sql);
+$result = $connection->query($sql);
 ?>
 
 <!DOCTYPE html>
